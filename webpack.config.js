1ccbf6d28ab5bd05b18a9913fc6552d94fac3f0d
hungryYang <ycr1714336458@163.com>
2017-04-11 15:01:16 +0800

    使用webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
 var path = require('path');
-var webpack = require('webpack');
-var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
 module.exports = {
     entry: './app/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
-        new uglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ]
+
 };
